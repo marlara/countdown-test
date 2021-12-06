abc9af1b48e9ffdda96142486e50790f933bc7e3
Lara Marziali <lara.marzi@gmail.com>
2021-12-06 13:57:38 +0100

    deleting fix

--- src/components/TheManageCountdown.vue
+++ src/components/TheManageCountdown.vue
@@ -6,11 +6,13 @@
         >
       <v-card-title>Countdown created</v-card-title>
     </v-img>
-    <v-card-text v-if="!countdowns">
+    <v-card-text v-if="!this.countdowns">
       <p>There are no countdowns yet! Have you started the server?</p>
     </v-card-text>
     <v-card-text v-else>
-      <p class="mb-4">If you want to delete a countdown use the button.</p>
+      <v-row class="mb-4">
+        <v-card-text>If you want to delete a countdown use the button.</v-card-text><v-spacer></v-spacer><v-btn elevation="2" @click="refreshCountdowns()">Reload the table</v-btn>
+      </v-row>
       <table>
         <thead>
         <tr>
@@ -21,13 +23,13 @@
           <th>Created at</th>
           <th>Delete</th>
         </tr>
-        <tr v-for="countdown in countdowns.data" :key="countdown.id">
-          <td>{{ countdown.id }}</td>
+        <tr v-for="(countdown, index) in this.countdowns.data" :key="index">
+          <td>{{ index+1 }}</td>
           <td>{{ countdown.hours }}</td>
           <td>{{ countdown.minutes }}</td>
           <td>{{ countdown.seconds }}</td>
           <td>{{ format_date(countdown.createdAt) }}</td>
-          <td><v-btn href="#" @click.prevent="deleteCountdown(countdown.id)">Delete</v-btn></td>
+          <td><v-btn href="#" @click.prevent="deleteCountdown(countdown.id, index)">Delete</v-btn></td>
           </tr>
         </thead>
         </table>
@@ -51,15 +53,21 @@ export default {
   },
 
   methods: {
+
+    async refreshCountdowns(){
+      this.countdowns = await api.getCountdowns()
+    },
+    
     format_date(value){
       if (value) {
         return moment(String(value)).format('YYYY- MM - DD  HH:MM:SS')
       }
     },
 
-    async deleteCountdown (id) {
+    async deleteCountdown (id, index) {
       if (confirm('Are you sure you want to delete this countdown?')) {
-        const response = await api.deleteCountdown(id);
+        const response = await api.deleteCountdown(id)
+        this.countdowns.data.splice(index, 1)
         return response
       }
     }
